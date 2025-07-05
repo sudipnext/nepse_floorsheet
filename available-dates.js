ea650b9040bcbd52c23b845c91d4d3584a5bb852
Sudip Parajuli <84361080+sudipnext@users.noreply.github.com>
2025-07-05 17:07:08 +0545

    Update available-dates.js: 2025-07-05

---
+++ available-dates.js
@@ -0,0 +1,12 @@
+// This file lists all available CSV files in the floorsheet folder
+// Updated automatically by the Lambda function
+const AVAILABLE_DATES = [
+    "2025-07-05"
+];
+
+// Export for use in script.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = AVAILABLE_DATES;
+} else if (typeof window !== 'undefined') {
+    window.AVAILABLE_DATES = AVAILABLE_DATES;
+}
